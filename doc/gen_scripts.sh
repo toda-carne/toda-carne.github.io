4bf1a80967e58450c6008f8c5e0950d6f64e37f7
Jose Luis Quiroga <joseluisquiroga@yahoo.com>
2022-04-17 15:47:31 -0500

    c109. ed doc

---
+++ doc/gen_scripts.sh
@@ -0,0 +1,8 @@
+pandoc -f odt -t markdown -o $1.md $1
+sed -i 's/\[\]{#anchor-[[:digit:]]*}//g' $1.md
+sed -i 's/^### /\nINICIO_SCRIPT\n### /' $1.md
+mkdir scripts
+mv $1.md scripts
+cd scripts
+csplit -z $1.md '/INICIO_SCRIPT/' '{*}'
+
