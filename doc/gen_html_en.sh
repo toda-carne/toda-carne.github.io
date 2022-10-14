f9e4a6c0908e38bad7476b435d1f1ad17c546d98
Jose Luis Quiroga <joseluisquiroga@yahoo.com>
2022-10-14 07:15:48 -0500

    c131. ed doc

--- doc/gen_html_en.sh
+++ doc/gen_html_en.sh
@@ -1,1 +1,9 @@
-pandoc -f odt -t html -o BOOK_ALL_FLESH.html BOOK_ALL_FLESH.odt --toc -s --toc-depth=6 --metadata title="All Flesh"
+
+pandoc -f odt -t html -o ALL_FLESH_BOOK.html ALL_FLESH_BOOK.odt --toc -s --toc-depth=6 --metadata title="All Flesh"
+tag=nav
+sed -n "/<$tag/,/<\/$tag>/p" ALL_FLESH_BOOK.html > AF_tmp_nav
+pup -f AF_tmp_nav > AF_nav.snippet
+rm AF_tmp_nav
+sed -i 's/<head>/<head>\n\t<meta charset="utf-8">/' AF_nav.snippet
+pandoc -f odt -t html -o AF_content.snippet ALL_FLESH_BOOK.odt
+
