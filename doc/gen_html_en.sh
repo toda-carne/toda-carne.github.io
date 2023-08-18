34439ad6d61ccab643648e0a403b0acefcb25df8
Jose Luis Quiroga <joseluisquiroga@yahoo.com>
2023-08-18 13:02:47 -0500

    c147. odt WITHOUT sections.

--- doc/gen_html_en.sh
+++ doc/gen_html_en.sh
@@ -1,11 +1,12 @@
 
 ODT_SRC=./SRC/ALL_FLESH_EBOOK.odt
+WEB_DIR=web
 
-pandoc -f odt -t html -o ALL_FLESH_BOOK.html $ODT_SRC --toc -s --toc-depth=6 --metadata title="All Flesh"
+pandoc -f odt -t html -o $WEB_DIR/all_flesh.html $ODT_SRC --toc -s --toc-depth=6 --metadata title="All Flesh"
 tag=nav
-sed -n "/<$tag/,/<\/$tag>/p" ALL_FLESH_BOOK.html > AF_tmp_nav
-pup -f AF_tmp_nav > nav_all_flesh.mustache
-rm AF_tmp_nav
-sed -i 's/<head>/<head>\n\t<meta charset="utf-8">/' nav_all_flesh.mustache
-pandoc -f odt -t html -o text_all_flesh.mustache $ODT_SRC
+sed -n "/<$tag/,/<\/$tag>/p" $WEB_DIR/all_flesh.html > $WEB_DIR/AF_tmp_nav
+pup -f $WEB_DIR/AF_tmp_nav > $WEB_DIR/nav_all_flesh.mustache
+rm $WEB_DIR/AF_tmp_nav
+sed -i 's/<head>/<head>\n\t<meta charset="utf-8">/' $WEB_DIR/nav_all_flesh.mustache
+pandoc -f odt -t html -o $WEB_DIR/text_all_flesh.mustache $ODT_SRC
 
--- doc/gen_html_es.sh
+++ doc/gen_html_es.sh
@@ -1,11 +1,12 @@
 
 ODT_SRC=./SRC/TODA_CARNE_EBOOK.odt
+WEB_DIR=web
 
-pandoc -f odt -t html -o LIBRO_TODA_CARNE.html $ODT_SRC --toc -s --toc-depth=6 --metadata title="Toda Carne"
+pandoc -f odt -t html -o $WEB_DIR/todacarne.html $ODT_SRC --toc -s --toc-depth=6 --metadata title="Toda Carne"
 tag=nav
-sed -n "/<$tag/,/<\/$tag>/p" LIBRO_TODA_CARNE.html > TC_tmp_nav
-pup -f TC_tmp_nav > nav_todacarne.mustache
-rm TC_tmp_nav
-sed -i 's/<head>/<head>\n\t<meta charset="utf-8">/' nav_todacarne.mustache
-pandoc -f odt -t html -o text_todacarne.mustache $ODT_SRC
+sed -n "/<$tag/,/<\/$tag>/p" $WEB_DIR/todacarne.html > $WEB_DIR/TC_tmp_nav
+pup -f $WEB_DIR/TC_tmp_nav > $WEB_DIR/nav_todacarne.mustache
+rm $WEB_DIR/TC_tmp_nav
+sed -i 's/<head>/<head>\n\t<meta charset="utf-8">/' $WEB_DIR/nav_todacarne.mustache
+pandoc -f odt -t html -o $WEB_DIR/text_todacarne.mustache $ODT_SRC
 
