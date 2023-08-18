0d20a7e12769e40aa4646485c1bb332a7fbc87cd
Jose Luis Quiroga <joseluisquiroga@yahoo.com>
2023-08-18 09:51:19 -0500

    c146.

--- doc/gen_html_en.sh
+++ doc/gen_html_en.sh
@@ -4,8 +4,8 @@ ODT_SRC=./SRC/ALL_FLESH_EBOOK.odt
 pandoc -f odt -t html -o ALL_FLESH_BOOK.html $ODT_SRC --toc -s --toc-depth=6 --metadata title="All Flesh"
 tag=nav
 sed -n "/<$tag/,/<\/$tag>/p" ALL_FLESH_BOOK.html > AF_tmp_nav
-pup -f AF_tmp_nav > AF_nav.snippet
+pup -f AF_tmp_nav > nav_all_flesh.mustache
 rm AF_tmp_nav
-sed -i 's/<head>/<head>\n\t<meta charset="utf-8">/' AF_nav.snippet
-pandoc -f odt -t html -o AF_content.snippet $ODT_SRC
+sed -i 's/<head>/<head>\n\t<meta charset="utf-8">/' nav_all_flesh.mustache
+pandoc -f odt -t html -o text_all_flesh.mustache $ODT_SRC
 
--- doc/gen_html_es.sh
+++ doc/gen_html_es.sh
@@ -4,8 +4,8 @@ ODT_SRC=./SRC/TODA_CARNE_EBOOK.odt
 pandoc -f odt -t html -o LIBRO_TODA_CARNE.html $ODT_SRC --toc -s --toc-depth=6 --metadata title="Toda Carne"
 tag=nav
 sed -n "/<$tag/,/<\/$tag>/p" LIBRO_TODA_CARNE.html > TC_tmp_nav
-pup -f TC_tmp_nav > TC_nav.snippet
+pup -f TC_tmp_nav > nav_todacarne.mustache
 rm TC_tmp_nav
-sed -i 's/<head>/<head>\n\t<meta charset="utf-8">/' TC_nav.snippet
-pandoc -f odt -t html -o TC_content.snippet $ODT_SRC
+sed -i 's/<head>/<head>\n\t<meta charset="utf-8">/' nav_todacarne.mustache
+pandoc -f odt -t html -o text_todacarne.mustache $ODT_SRC
 
