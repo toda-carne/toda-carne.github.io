17e5beeb218f2d36118eb999077f2f8b8a6ef7c4
Jose Luis Quiroga <joseluisquiroga@yahoo.com>
2024-01-08 16:44:08 -0500

    c154. added FIX_refs to es and es

---
+++ website_src/es_mustache/FIX_refs.sh
@@ -0,0 +1,5 @@
+
+sed -i -E 's/([href|id]="[^.]*)\.([^"]*")/\1_DOT_\2/g' nav_todacarne.mustache
+sed -i -E 's/([href|id]="[^.]*)\.([^"]*")/\1_DOT_\2/g' nav_todacarne.mustache
+sed -i -E 's/([href|id]="[^.]*)\.([^"]*")/\1_DOT_\2/g' text_todacarne.mustache
+sed -i -E 's/([href|id]="[^.]*)\.([^"]*")/\1_DOT_\2/g' text_todacarne.mustache
