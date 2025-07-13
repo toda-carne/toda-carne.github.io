79e37f1e7452b864c78f177faec70a0552b0eaae
Jose Luis Quiroga <joseluisquiroga@yahoo.com>
2025-07-12 19:24:28 -0500

    c375. fixed js_scods.

---
+++ strong_bible_selector/data/scod_tabs/clean.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+CURR_DIR=$(pwd)
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo "clean ${1} using "${BASH_SOURCE[0]}
+
+cd $SCRIPT_DIR
+
+rm *_ref_scodes.tab
+
+echo "Finished clean ----------------------------- "
+
+cd ${CURR_DIR}
